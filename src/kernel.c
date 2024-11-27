631d1c18f0299979db4eddd3d43393ad1d7f86dc
AlexisPravaig <vasseur.yves@pravaig.com>
2024-11-27 14:24:21 +0530

    fix(kernel): remove debug code

--- src/kernel.c
+++ src/kernel.c
@@ -4,59 +4,6 @@ void main(void)
 {
     set_color(COLOR_WHITE, COLOR_BLACK);
     clear_screen();
-
-    print_str("1\n");
-
-    print_str("2\n");
-
-    print_str("3\n");
-
-    print_str("4\n");
-
-    print_str("5\n");
-    print_str("6\n");
-
-    print_str("7\n");
-
-    print_str("8\n");
-
-    print_str("9\n");
-
-    print_str("10\n");
-
-    print_str("11\n");
-
-    print_str("12\n");
-
-    print_str("13\n");
-
-    print_str("14\n");
-
-    print_str("15\n");
-
-    print_str("16\n");
-
-    print_str("17\n");
-
-    print_str("18\n");
-
-    print_str("19\n");
-
-    print_str("20\n");
-
-    print_str("21\n");
-
-    print_str("22\n");
-
-    print_str("23\n");
-
-    print_str("24\n");
-
-    print_str("25\n");
-
-    print_str("26\n");
-    print_str("27\n");
-    print_str("28");
-
-    // print_str("My name's Alexis VASSEUR");
+    print_str("Hello, from a new S3E!\n");
+    print_str("My name's Alexis VASSEUR");
 }
